ba63b361995da22038531ae87a6d606f72da9ae7
stvujic <83906734+stvujic@users.noreply.github.com>
2025-05-28 15:58:57 +0200

    * Added AdminMiddleware and protected /admin/dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,10 +20,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/user/dashboard', function () {
         return view('user.dashboard');
     })->name('user.dashboard');
-
-    Route::get('/admin/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
 });
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', AdminMiddleware::class])->name('admin.dashboard');
 
 require __DIR__.'/auth.php';
