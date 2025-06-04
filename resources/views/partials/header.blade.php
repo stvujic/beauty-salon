8f702981d84d3513a299074c8fafeafcb56a15f8
stvujic <83906734+stvujic@users.noreply.github.com>
2025-06-04 13:47:20 +0200

    * Simplify header navigation for admin users to show only key admin links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,73 +2,86 @@
     <h1 id="logo"><a href="/">KRALJEVSKA MADEROTERAPIJA</a></h1>
     <nav id="nav">
         <ul>
-
-
             @auth
                 @if(auth()->user()->is_admin)
                     {{-- Samo admin vidi ovo --}}
                     <li><a href="{{ route('admin.appointments') }}">Sve rezervacije</a></li>
                     <li><a href="{{ route('admin.users.index') }}">Korisnici</a></li>
-                    <li><a href="{{route('admin.packages.index')}}">Paketi</a></li>
-                    <li><a href="{{route('admin.comments.index')}}">Admin Komentari</a></li>
+                    <li><a href="{{ route('admin.packages.index') }}">Paketi</a></li>
+                    <li><a href="{{ route('admin.comments.index') }}">Admin Komentari</a></li>
                 @else
                     {{-- Samo običan korisnik vidi ovo --}}
-
                     <li><a href="{{ route('appointments.create') }}">Rezervacije</a></li>
                     <li><a href="{{ route('appointments.index') }}">Moje rezervacije</a></li>
-                @endif
 
+                    <li><a href="/">Glavna</a></li>
+                    <li><a href="{{ url('/#one') }}" class="scrolly">O nama</a></li>
+                    <li><a href="{{ route('public.comments') }}">Komentari</a></li>
+                    <li><a href="/">Galerija</a></li>
+                    <li><a href="/">Vauceri</a></li>
+                    <li><a href="{{ route('public.packages') }}">Cenovnik</a></li>
 
-
-            @else
-                {{-- Gost vidi ovo --}}
-                <li><a href="{{ route('appointments.create') }}">Rezervacije</a></li>
-            @endauth
-
-            <li><a href="/">Glavna</a></li>
-            <li><a href="{{ url('/#one') }}" class="scrolly">O nama</a></li>
-            <li><a href="{{route('public.comments')}}">Komentari</a></li>
-            <li><a href="/">Galerija</a></li>
-            <li><a href="/">Vauceri</a></li>
-            <li><a href="{{ route('public.packages') }}">Cenovnik</a></li>
-
-
-            {{-- Dropdown posle cu ga iskorisiti --}}
-            <li>
-                <a href="#">Paketi</a>
-                <ul>
-                    <li><a href="/left-sidebar">Left Sidebar</a></li>
-                    <li><a href="/right-sidebar">Right Sidebar</a></li>
-                    <li><a href="/no-sidebar">No Sidebar</a></li>
+                    {{-- Dropdown --}}
                     <li>
-                        <a href="#">Submenu</a>
+                        <a href="#">Paketi</a>
                         <ul>
-                            <li><a href="#">Option 1</a></li>
-                            <li><a href="#">Option 2</a></li>
-                            <li><a href="#">Option 3</a></li>
-                            <li><a href="#">Option 4</a></li>
+                            <li><a href="/left-sidebar">Left Sidebar</a></li>
+                            <li><a href="/right-sidebar">Right Sidebar</a></li>
+                            <li><a href="/no-sidebar">No Sidebar</a></li>
+                            <li>
+                                <a href="#">Submenu</a>
+                                <ul>
+                                    <li><a href="#">Option 1</a></li>
+                                    <li><a href="#">Option 2</a></li>
+                                    <li><a href="#">Option 3</a></li>
+                                    <li><a href="#">Option 4</a></li>
+                                </ul>
+                            </li>
                         </ul>
                     </li>
-                </ul>
-            </li>
 
-            {{-- Ostali dizajn elementi --}}
-            <li><a href="/elements">Elements</a></li>
+                    <li><a href="/elements">Elements</a></li>
+                @endif
 
-            {{-- Autentikacija --}}
-            @auth
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <button type="submit" class="button">Logout</button>
                     </form>
                 </li>
-            @endauth
+            @else
+                {{-- Gost vidi ovo --}}
+                <li><a href="{{ route('appointments.create') }}">Rezervacije</a></li>
+                <li><a href="/">Glavna</a></li>
+                <li><a href="{{ url('/#one') }}" class="scrolly">O nama</a></li>
+                <li><a href="{{ route('public.comments') }}">Komentari</a></li>
+                <li><a href="/">Galerija</a></li>
+                <li><a href="/">Vauceri</a></li>
+                <li><a href="{{ route('public.packages') }}">Cenovnik</a></li>
 
-            @guest
+                <li>
+                    <a href="#">Paketi</a>
+                    <ul>
+                        <li><a href="/left-sidebar">Left Sidebar</a></li>
+                        <li><a href="/right-sidebar">Right Sidebar</a></li>
+                        <li><a href="/no-sidebar">No Sidebar</a></li>
+                        <li>
+                            <a href="#">Submenu</a>
+                            <ul>
+                                <li><a href="#">Option 1</a></li>
+                                <li><a href="#">Option 2</a></li>
+                                <li><a href="#">Option 3</a></li>
+                                <li><a href="#">Option 4</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li><a href="/elements">Elements</a></li>
+
                 <li><a href="{{ route('login') }}" class="button">Login</a></li>
                 <li><a href="{{ route('register') }}" class="button primary">Sign Up</a></li>
-            @endguest
+            @endauth
         </ul>
     </nav>
 </header>
